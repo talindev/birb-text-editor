20bde8788cbc9d01d56cb8bffa89885fb2627548
Italo Garcia <talin.dev1@gmail.com>
2026-07-14 22:11:20 -0300

    step 1 progress

--- kilo.c
+++ kilo.c
@@ -1,6 +1,26 @@
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode(void) {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(void) {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(void) {
+	enableRawMode();
+
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
